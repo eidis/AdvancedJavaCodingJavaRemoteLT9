98c0c766b351a35ea6793ec402f8d7ecf5ce8e05
Kęstutis Triponis <kestutis.triponis@gmail.com>
2021-08-01 08:30:29 +0200

    Implement HashSet using hashtable

--- src/main/java/sda/SDAHashSet.java
+++ src/main/java/sda/SDAHashSet.java
@@ -1,45 +1,48 @@
 package sda;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SDAHashSet<E> {
 
-    private final List<E> list = new ArrayList<>();
+    private final Map<Integer, E> hashtable = new HashMap<>();
 
     public int size() {
-        return list.size();
+        return hashtable.size();
     }
 
     public boolean contains(Object o) {
-        return list.contains(o);
+        return hashtable.containsKey(o.hashCode());
     }
 
     public boolean add(E e) {
+        int hash = e.hashCode();
         if (!contains(e)) {
-            return list.add(e);
+            hashtable.put(hash, e);
+            return true;
         }
         return false;
     }
 
     public boolean remove(Object o) {
-        return list.remove(o);
+        return hashtable.remove(o.hashCode()) != null;
     }
 
     public void clear() {
-        list.clear();
+        hashtable.clear();
     }
 
     @Override
     public String toString() {
-        return list.toString();
+        return hashtable.toString();
     }
 
     public static void main(String[] args) {
         SDAHashSet<String> set = new SDAHashSet<>();
+        set.add("x");
         set.add("a");
         set.add("a");
-        set.add("b");
+        set.add("bb");
         set.add("c");
         System.out.println(set);
         set.remove("a");
